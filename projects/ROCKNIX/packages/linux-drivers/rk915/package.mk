b1c554a0489ed7f1dc74575eae6d5dfd2dbbcee0
kyle <kyle@autonomous.ai>
2026-06-06 05:22:11 +0700

    RK3326: add rk915 SDIO WiFi support for kernel 6.12
    - package.mk: use AveyondFly/rk915 (has 6.12 compat fixes)
    - DTS: configure sdio for rk915 (supports-rk912, cap-sd-highspeed,
      max-frequency 50MHz, no-sd/no-mmc, wifi@1 IRQ node on GPIO0 PA5)
    - kernel patch: add MMC_CAP2_WIFI_RK912 cap flag; parse supports-rk912
      DT prop; relax SDIO CIS validation; skip resume power-up; dw_mmc
      PRV_DAT_WAIT for SD_IO_RW_DIRECT; disable low-power clock gating
    - options: add rk915 to ADDITIONAL_DRIVERS

--- projects/ROCKNIX/packages/linux-drivers/rk915/package.mk
+++ projects/ROCKNIX/packages/linux-drivers/rk915/package.mk
@@ -2,10 +2,10 @@
 # Copyright (C) 2025-present ROCKNIX (https://github.com/ROCKNIX)
 
 PKG_NAME="rk915"
-PKG_VERSION="a6b2c7d3e8f1"
+PKG_VERSION="e2fd61651c272aab7bd7e76ba05e5682d7c1d211"
 PKG_LICENSE="GPL"
-PKG_SITE="https://github.com/rockchip-linux/rkwifibt"
-PKG_URL="https://github.com/rockchip-linux/rkwifibt/archive/${PKG_VERSION}.tar.gz"
+PKG_SITE="https://github.com/AveyondFly/rk915"
+PKG_URL="https://github.com/AveyondFly/rk915/archive/${PKG_VERSION}.tar.gz"
 PKG_LONGDESC="Rockchip RK915 SDIO WiFi driver (out-of-tree)"
 PKG_TOOLCHAIN="manual"
 PKG_IS_KERNEL_PKG="yes"
@@ -15,16 +15,16 @@ pre_make_target() {
 }
 
 make_target() {
-  kernel_make ARCH=${TARGET_KERNEL_ARCH} \
-              KSRC=$(kernel_path) \
-              CROSS_COMPILE=${TARGET_KERNEL_PREFIX} \
-              -C ${PKG_BUILD}/drivers/net/wireless/rockchip_wlan/rk915 \
-              KDIR=$(kernel_path) \
-              modules
+  kernel_make -C $(kernel_path) M=${PKG_BUILD} CONFIG_RK915=m modules
 }
 
 makeinstall_target() {
-  mkdir -p ${INSTALL}/$(get_full_module_dir)/kernel/drivers/net/wireless/rockchip_wlan/rk915
-  cp ${PKG_BUILD}/drivers/net/wireless/rockchip_wlan/rk915/rk915.ko \
-     ${INSTALL}/$(get_full_module_dir)/kernel/drivers/net/wireless/rockchip_wlan/rk915/
+  local moddir="${INSTALL}/$(get_full_module_dir)/kernel/drivers/net/wireless/rockchip_wlan/rk915"
+  mkdir -p "${moddir}"
+  cp ${PKG_BUILD}/rk915.ko "${moddir}/"
+
+  local fwdir="${INSTALL}/$(get_full_firmware_dir)"
+  mkdir -p "${fwdir}"
+  cp ${PKG_BUILD}/firmware/rk915_fw.bin "${fwdir}/"
+  cp ${PKG_BUILD}/firmware/rk915_patch.bin "${fwdir}/"
 }
