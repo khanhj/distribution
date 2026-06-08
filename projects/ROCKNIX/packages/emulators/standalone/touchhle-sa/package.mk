ebe2d39635e30908836e58b849f569b5486104e3
kyle <kyle@autonomous.ai>
2026-06-08 07:51:53 +0700

    fix touchhle-sa: add git+tag keys to cargo source section for 1.94
    Co-Authored-By: kyle-aus <genesis0000@bvm.network>

--- projects/ROCKNIX/packages/emulators/standalone/touchhle-sa/package.mk
+++ projects/ROCKNIX/packages/emulators/standalone/touchhle-sa/package.mk
@@ -28,6 +28,8 @@ post_unpack() {
 replace-with = "vendored-sources"
 
 [source."git+https://github.com/touchHLE/rust-sdl2?tag=touchHLE-3#b67f98fe6a147773b6f05d50ae775d9fe16561e7"]
+git = "https://github.com/touchHLE/rust-sdl2"
+tag = "touchHLE-3"
 replace-with = "vendored-sources"
 
 [source.vendored-sources]
